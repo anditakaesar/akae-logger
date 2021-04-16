1bd4f75a03b51b30fa0786f6b5f495cc9122a7c0
Andita Kaesar <anditakaesar@gmail.com>
2021-04-16 19:11:23 +0700

    initialize new app

---
+++ src/app.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import { json, urlencoded } from 'body-parser'
+import compression from 'compression'
+import helmet from 'helmet'
+
+// initialize express
+const app = express()
+
+// middleware
+app.use(json())
+app.use(urlencoded({ extended: true }))
+app.use(compression())
+app.use(
+    helmet({
+        contentSecurityPolicy: false,
+    }),
+)
+// app.use(cors())
+
+// routers
+app.use('/', (req, res) => {
+    res.json({
+        message: 'Ok'
+    })
+})
+
+// error handling
+app.use((err, req, res, next) => {
+    // log ?
+    if (err) {
+        res.json({
+            message: err.message,
+        })
+    } else {
+        next()
+    }
+})
+
+export default app
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import app from "./app";
+
+app.listen(4000, () => {
+    // log ?
+    console.log('app start at port 4000')
+})
