699890541e1a77c8d7ec757d0ca3ff1b4b5b5798
Andita Kaesar <anditakaesar@gmail.com>
2021-04-16 20:52:45 +0700

    initial sequelize, first success saving

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@ import express from 'express'
 import { json, urlencoded } from 'body-parser'
 import compression from 'compression'
 import helmet from 'helmet'
+import helper from './helper'
+import moment from 'moment'
+import logger from './logger'
 
 // initialize express
 const app = express()
+const { Record } = helper.db
 
 // middleware
 app.use(json())
@@ -21,6 +25,30 @@ app.use(
 app.get('/', (req, res) => {
   res.json({
     message: 'Ok'
+  })
+})
+
+app.post('/', (req, res, next) => {
+
+  process.nextTick(() => {
+    const now = moment()
+    const newRecord = {
+      record: {
+        message: 'this',
+      },
+      createdAt: now,
+      updatedAt: now,
+    }
+
+    Record.create(newRecord)
+      .then((r) => {
+        res.json({
+          message: 'New Record saved'
+        })
+      })
+      .catch((err) => {
+        next(err)
+      })
   })
 })
 
